ed60d3aeccb14cd6aaf29bfbdccbf8d425edba45
MahmoudAlsmairat <malsmairat@aspire-services.net>
2022-07-25 17:00:42 +0300

    enhanced modal component structure

--- src/components/shared/Modal/Modal.js
+++ src/components/shared/Modal/Modal.js
@@ -2,16 +2,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 import styles from "./styles.module.scss";
 import useModal from "../../../hooks/useModal";
-import CommonButton from "../Button/Button";
 import Animate from "../Animate/Animate";
-const {
-  modal,
-  modalOverlay,
-  modalHeader,
-  modalBody,
-  modalFooter,
-  modalButton,
-} = styles;
+import ModalHeader from "./ModalHeader/ModalHeader";
+import ModalBody from "./ModalBody/ModalBody";
+import ModalFooter from "./ModalFooter/ModalFooter";
+const { modal, modalOverlay } = styles;
 
 const Modal = () => {
   const { showModal, modalContent } = useSelector((state) => state.modal);
@@ -24,23 +19,13 @@ const Modal = () => {
       {showModal && <div className={modalOverlay}></div>}
       <Animate animationType="scaleUpDown" showsIn={showModal}>
         <div className={modal}>
-          <div className={modalHeader}> {title} </div>
-          <div className={modalBody}> {message} </div>
-          <div className={modalFooter}>
-            <CommonButton
-              onClick={hideModal}
-              color={isConfirmation ? "textPrimary" : "primary"}
-              className={modalButton}
-              label={"Close"}
-            />
-            {isConfirmation && (
-              <CommonButton
-                className={modalButton}
-                onClick={confirmButtonClick}
-                label={"Confirm"}
-              />
-            )}
-          </div>
+          <ModalHeader title={title} />
+          <ModalBody message={message} />
+          <ModalFooter
+            isConfirmation={isConfirmation}
+            hideModal={hideModal}
+            confirmButtonClick={confirmButtonClick}
+          />
         </div>
       </Animate>
     </>
